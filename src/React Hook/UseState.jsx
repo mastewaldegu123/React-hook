0fc107a1906407421a82ec396318c863068cad5c
mastewaldegu123 <mastewaldegu0@gmail.com>
2023-03-02 12:00:05 +0300

    third commit

--- src/React Hook/UseState.jsx
+++ src/React Hook/UseState.jsx
@@ -2,9 +2,10 @@ import React,{useState} from 'react'
 
 function UseState() {
   const [count,setCount]= useState(0)
+  const [ showtext ,setShowText]=useState(false)
   const handleclick=()=>{
     setCount(count + 1)
-    
+    setShowText(!showtext)
 
 
 
@@ -14,6 +15,8 @@ function UseState() {
        
  <h4 style={{width:'50px'}}>{count}</h4>
 <button style={{ backgroundColor:'skyblue' ,width:'100px'}  } onClick={handleclick }>click</button>
+
+{showtext && <p>the text is shown </p>  }
     </div>
   )
 }
